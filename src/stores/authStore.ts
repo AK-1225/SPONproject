142e84aabedede6bc06d63c2d18eb0b9ab371223
AK-1225 <o.akak0759@gmail.com>
2026-01-15 20:02:33 +0900

    直し

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -90,9 +90,6 @@ export const useAuthStore = create<AuthState>()(
                             .eq('id', data.user.id)
                             .single()
 
-                        console.log("取得しようとしているID:", data.user.id);
-                        console.log("実際に取得できたプロフィール:", profile);
-
                         if (profile) {
                             const user: User = {
                                 id: profile.id,
